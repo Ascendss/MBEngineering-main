ffefa3fd5f388a4ab349f90d4b544e08108f5c33
Ascendss <redskinsfan675@gmail.com>
2025-05-21 20:41:33 -0400

    Create getProjects.js

---
+++ netlify/functions/getProjects.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+const path = require("path");
+const matter = require("gray-matter");
+
+exports.handler = async () => {
+  try {
+    const dir = path.resolve(__dirname, "../../content/projects");
+    const files = fs.readdirSync(dir);
+
+    const projects = files
+      .filter(file => file.endsWith(".md"))
+      .map(file => {
+        const filePath = path.join(dir, file);
+        const content = fs.readFileSync(filePath, "utf8");
+        const { data } = matter(content);
+
+        return {
+          title: data.title,
+          description: data.description,
+          image: data.image,
+          link: data.link || "#",
+          slug: file.replace(".md", "")
+        };
+      });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(projects)
+    };
+
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message })
+    };
+  }
+};
