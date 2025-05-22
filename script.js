6ae9221567f2b18ba6a2244876d4ab9d1f93c927
Ascendss <redskinsfan675@gmail.com>
2025-05-21 20:53:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,21 @@
-const gallery = document.getElementById("project-gallery");
-
-// Fetch project data from Netlify Function
-fetch("/.netlify/functions/getProjects")
+fetch('/.netlify/functions/getProjects')
   .then(res => res.json())
   .then(projects => {
+    const gallery = document.querySelector('.gallery');
     gallery.innerHTML = "";
 
     projects.forEach(project => {
-      const card = document.createElement("div");
-      card.className = "project-card";
-
-      card.innerHTML = `
-        <a href="${project.link || '#'}" target="_blank">
+      const div = document.createElement("div");
+      div.className = "project-card";
+      div.innerHTML = `
+        <a href="${project.link || "#"}" target="_blank">
           <img src="${project.image}" alt="${project.title}" />
           <h3>${project.title}</h3>
           <p>${project.description}</p>
-        </a>
-      `;
-
-      gallery.appendChild(card);
+        </a>`;
+      gallery.appendChild(div);
     });
   })
   .catch(err => {
     console.error("Failed to load projects:", err);
-    gallery.innerHTML = "<p>Unable to load projects at this time.</p>";
   });
